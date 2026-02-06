2b98943f75323caea09f952328649085acacf08c
Ganjar AA <gaa.ammar@gmail.com>
2026-02-06 22:24:32 +0700

    fix: revert logging plugin to stabilize launch and bump to v1.0.9

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,11 +4,6 @@ pub fn run() {
     .plugin(tauri_plugin_updater::Builder::new().build())
     .plugin(tauri_plugin_process::init())
     .plugin(tauri_plugin_dialog::init())
-    .plugin(
-      tauri_plugin_log::Builder::default()
-        .level(log::LevelFilter::Info)
-        .build(),
-    )
     .setup(|_app| {
       Ok(())
     })
